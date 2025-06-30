1d19111bf2f9b351dfd16d4a8fec71524f77bf16
DucHuyFDev <leduchuy13803@gmail.com>
2025-06-30 08:28:53 +0700

    Bài 1 + 2

---
+++ Bai2_XML_XSD/ThucHanh2_THHTPM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ThucHanh2_THHTPM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
